84c4b6780baa56589d0e77bfed1a3ac868b46640
Srikanth D <15664534+srikanthd1981@users.noreply.github.com>
2018-10-21 09:22:18 -0700

    Initial version of a basic MEAN app

---
+++ app/javascripts/chirpApp.js
@@ -0,0 +1,27 @@
+//chirpApp.js
+var app = angular.module('chirpApp', []);
+
+app.controller('mainController', function($scope){
+	$scope.posts=[];
+	$scope.newPost={created_by:'',text:'',created_at:''};
+	
+	$scope.post=function(){
+		$scope.newPost.created_at=Date.now();
+		$scope.posts.push($scope.newPost);
+		$scope.newPost={created_by:'',text:'',created_at:''};
+	};
+});
+
+app.controller('authController',function($scope){
+	$scope.user={username:'',password:''};
+	$scope.error_message='';
+
+	$scope.register=function(){
+		$scope.error_message=$scope.user.username + " Registered";
+		$scope.user={username:'',password:''};
+	};
+	$scope.login=function(){
+		$scope.error_message="Login request for " + $scope.user.username ;
+		$scope.user={username:'',password:''};
+	};
+});
